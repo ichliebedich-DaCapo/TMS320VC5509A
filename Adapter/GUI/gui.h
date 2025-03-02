cb2b6298471d1a9b44b70455af94945ef2417fdd
fairy <3475397231@qq.com>
2025-03-03 01:30:08 +0800

    【开发】勉强添加了一个u8g2库，后面可以再做一个模拟器辅助开发

---
+++ Adapter/GUI/gui.h
@@ -0,0 +1,18 @@
+//
+// Created by fairy on 2025/3/3 01:09.
+//
+#ifndef GUI_H
+#define GUI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zq_conf.h>
+
+    void GUI_Init();
+
+#ifdef __cplusplus
+}
+#endif
+#endif //GUI_H
