470ffe17f9f013b88f2df0f8402d104474a0342a
fairy <3475397231@qq.com>
2025-03-04 16:40:36 +0800

    【模拟器开发】设计一个可用的初步示例

--- App/9_LCD/ui/ui.c
+++ App/9_LCD/ui/ui.c
@@ -31,6 +31,7 @@ Oscilloscope osc;
 static uint8_t last_y = 0;
 static uint8_t x = 0;
 static char buf[10];
+static uint8_t cache[1024];
 
 void ui_init()
 {
@@ -40,7 +41,7 @@ void ui_init()
     /* 垂直网格 */
     for (x = 0; x < 128; x += 16)
     {
-        u8g2_DrawVLine(&u8g2, x, 8, 56);
+        u8g2_DrawVLine(&u8g2, x, 8, 48);
     }
 
     /* 水平网格 */
@@ -51,52 +52,42 @@ void ui_init()
     }
 
     /* 固定文字 */
-    u8g2_SetFont(&u8g2, u8g2_font_6x10_tf);
-    u8g2_DrawStr(&u8g2, 2, 63, "CH1");
-    u8g2_DrawStr(&u8g2, 110, 63, "RUN");
+    u8g2_SetFont(&u8g2, u8g2_font_3x5im_mr);
+    u8g2_DrawStr(&u8g2, 2, 64, "CH1");
+    u8g2_DrawStr(&u8g2, 112, 64, "RUN");
 
     /* 初始化波形缓冲区 */
     for (x = 0; x < 128; x++)
     {
         osc.waveform[x] = 32; // 初始居中
     }
-
-    u8g2_SendBuffer(&u8g2);
-    u8g2_SetAutoPageClear(&u8g2, 1);
+    // 发送缓冲区数据
+    memcpy(cache, u8g2.tile_buf_ptr, 1024);
 }
 
 
 void ui_handler()
 {
-
-
     /* 更新波形数据 */
     static uint16_t phase = 0;
     for (x = 0; x < 128; x++)
     {
-        osc.waveform[x] = 32 + (uint8_t) (30 * sin(phase * 0.1 + x * 0.2));
+        osc.waveform[x] = (uint8_t) ( 110 * (sin(phase * 0.1 + x * 0.2)+1.1));
     }
     phase++;
 
+
     /* 开始绘制流程 */
-    // u8g2_ClearBuffer(&u8g2);
-    u8g2_FirstPage(&u8g2);
-    do {
-    /* 绘制静态元素 */
-    u8g2_SetDrawColor(&u8g2, 1);
-    u8g2_DrawHLine(&u8g2, 0, 8, 128);
+    // 接收缓冲区数据
+    memcpy(u8g2.tile_buf_ptr, cache, 1024);
 
     /* 动态波形绘制 */
     u8g2_SetDrawColor(&u8g2, 1);
-    last_y = osc.waveform[0];
+    last_y = osc.waveform[0]*48/255.0f;
     for (x = 1; x < 128; x++)
     {
-        u8g2_DrawLine(&u8g2,
-                      x - 1,
-                      63 - last_y, // Y轴翻转
-                      x,
-                      63 - osc.waveform[x]);
-        last_y = osc.waveform[x];
+        u8g2_DrawLine(&u8g2,x - 1, 56 - last_y,x,56 - osc.waveform[x]*48/255.0f);
+        last_y = osc.waveform[x]*48/255.0f;
     }
 
     /* 实时参数显示 */
@@ -104,7 +95,5 @@ void ui_handler()
     sprintf(buf, "%3dHz", 1000);
     u8g2_DrawStr(&u8g2, 50, 8, buf);
 
-    } while (u8g2_NextPage(&u8g2));
-
-    // u8g2_SendBuffer(&u8g2);
+    u8g2_SendBuffer(&u8g2);
 }
